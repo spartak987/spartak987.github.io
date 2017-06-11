c44e5a0dd7d71271d8c7f1889be04dba136391c0
spartak987 <abalmaz.spartak@yandex.ua>
2017-06-11 20:07:00 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,36 @@ var gulp = require('gulp');
 		pug = require('gulp-pug');
 		browserSync = require('browser-sync');
 
-gulp.task('sass', function(){
-	gulp.src('layout/sass/*.sass')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(''));
+gulp.task('pug', function() {
+  gulp.src(['pug/index.pug', 'pug/my_works.pug'])
+    .pipe(pug({
+      pretty: true
+    }))
+  .pipe(gulp.dest(''));
 });
 
-gulp.task('pug', function() {
-  gulp.src(['layout/pug/index.pug', 'layout/pug/my_works.pug'])
-  	.pipe(pug({
-  		pretty: true
-  	}))
-	.pipe(gulp.dest(''));
+gulp.task('sass', function(){
+	gulp.src('sass/*.sass')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(''))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('watch', function(){
-	gulp.watch('layout/pug/*.pug', ['pug']);
-	gulp.watch('layout/sass/*.sass', ['sass']);
+	gulp.watch('sass/*.sass', ['sass']);
+  gulp.watch('pug/*.pug', ['pug']);
+  gulp.watch('*.html', browserSync.reload);
 });
 
-gulp.task('default', ['pug', 'sass', 'watch']);
-
-gulp.task('browser-sync', function () {
-  var files = [
-    'layout/*.html',
-    'layout/*.css',
-    'layout/js/*.js'
-  ];
-  
-  browserSync.init(files, {
+gulp.task('browser-sync', function() {
+  browserSync({
     server: {
-      baseDir: './app'
-    }
+      baseDir: ''
+    },
+    notify: false,
+    // tunnel: true,
+    // tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
   });
 });
+
+gulp.task('default', ['pug','sass','watch','browser-sync']);
